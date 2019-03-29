406358f64bcaa67d62982921461d4aa2d66f67c3
garo <gabrielerovi@MBP-di-Gabriele.mobile.usilu.net>
2019-03-29 14:42:36 +0100

    mars_connectivity.hpp constructor by reference

--- mars_entity.hpp
+++ mars_entity.hpp
@@ -6,6 +6,22 @@
 #include <iostream>
 #include <vector>
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////// Written by Gabriele Rovi (April 2019)																							////////
+////// We define the following class:																									////////
+////// Entity<Integer Dim, Integer ManifoldDim, Integer EntityDim>																		////////
+////// Given a mesh, with elements whose dimension is ManifoldDim, defined in a Dim-dimensional space									////////
+////// We want to define the entities of dimension 0<=EntityDim<=ManifoldDim  						     								////////
+////// The class has two fields:   																										////////														
+//////  1) entity_2_elem_: for each entity id, returns a 2-D array A such that:															////////
+//////  A[0]= id of one of the element to wich entity belongs																			////////
+//////  A[1]= local number of the entity inside the element A[0] 																		////////
+//////  2) elem_2_entity: for each element it returns a entity_combinations-D array containing all the ids of the type of the entity    ////////									////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 namespace mars {
 
 template<Integer Dim, Integer ManifoldDim>
