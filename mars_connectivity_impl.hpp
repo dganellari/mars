a429b82c4d322a3e7e2e6a3f3953a27881024968
garo <gabrielerovi@MBP-di-Gabriele.mobile.usilu.net>
2019-04-01 10:27:05 +0200

    modification after first revision

--- mars_connectivity_impl.hpp
+++ mars_connectivity_impl.hpp
@@ -19,7 +19,7 @@
 ////// Example 3) Dim=ManifoldDim=3D, e1=triangle, SubEntityDimFrom=2, e2=triangle:														////////
 //////            all the triangles e2 which share a triangle with the triangle e1 -> e1=e2, known a priori								////////
 ////// Rules: 1) if SubEntityDimFrom > EntityDimFrom: the set is empty																	////////
-//////        2) if SubEntityDimFrom = EntityDimFrom: the set is the entity e1 itself													////////
+//////        2) if SubEntityDimFrom = EntityDimFrom == EntityDimTo: the set is the entity e1 itself	 								////////
 //////        3) The class with EntityDimFrom=SubEntityDimFrom=0 and EntityDimTo=ManifoldDim is defined separately						////////
 //////           Indeed it will store a vector of vectors, whose component (the node id) returns the neighborhood elements              ////////
 //////        4) For all the other classes, given the index entity, the connectivity is computed on the fly   							////////
@@ -39,37 +39,38 @@ inline std::vector<Integer> Connectivity<Dim,ManifoldDim,EntityDimFrom,SubEntity
 {
 
 
-    std::vector<Integer> FromTo;
+    std::vector<Integer> fromto;
     
-    assert(Dim>=0 && " the space dimension must be non negative ");
-    assert(ManifoldDim>=0 && " the manifold dimension must be non negative ");
-    assert(Dim>=ManifoldDim && " the space dimension must be greater than the manifold dimension ");
-    
-    assert(EntityDimFrom>=0 && " the entityfrom dimension must be non negative ");
-    assert(ManifoldDim>=EntityDimFrom && " the manifold dimension must be greater than the entityfrom dimension ");
-    assert(SubEntityDimFrom>=0 && " the subentityfrom dimension must be non negative ");
-    assert(ManifoldDim>=SubEntityDimFrom && " the manifold dimension must be greater than the subentityfrom dimension ");
-    
-    assert(EntityDimTo>=0 && " the entityto dimension must be non negative ");
-    assert(ManifoldDim>=EntityDimTo && " the manifold dimension must be greater than the entityto dimension ");
+    static_assert(Dim>=0 , " the space dimension must be non negative ");
+    static_assert(ManifoldDim>=0 , " the manifold dimension must be non negative ");
+    static_assert(Dim>=ManifoldDim , " the space dimension must be greater than or equal to than the manifold dimension ");   
+    static_assert(EntityDimFrom>=0 , " the entityfrom dimension must be non negative ");
+    static_assert(ManifoldDim>=EntityDimFrom , " the manifold dimension must be greater than or equal to the entityfrom dimension ");
+    static_assert(SubEntityDimFrom>=0 , " the subentityfrom dimension must be non negative ");
+    static_assert(ManifoldDim>=SubEntityDimFrom , " the manifold dimension must be greater than or equal to the subentityfrom dimension ");   
+    static_assert(EntityDimTo>=0 , " the entityto dimension must be non negative ");
+    static_assert(ManifoldDim>=EntityDimTo , " the manifold dimension must be greater than or equal to the entityto dimension ");
     
     // rule 1)
     if(SubEntityDimFrom>EntityDimFrom || SubEntityDimFrom > EntityDimTo)
-         return FromTo;
+         return fromto;
          
     // rule 2)
     else if(SubEntityDimFrom==EntityDimFrom && EntityDimFrom==EntityDimTo)
-        {FromTo.resize(1);
-         FromTo[0]=index_from;
-         return FromTo;}
+        {
+         fromto.resize(1);
+         fromto[0]=index_from;
+         return fromto;
+         }
          
     else
-       {Integer entity_nodes_from[EntityDimFrom+1];
+       {
+        Integer entity_nodes_from[EntityDimFrom+1];
         Integer entity_nodes_to[EntityDimTo+1];
         Integer common_nodes;
 		std::vector<Integer> el_neighs;
-		const Integer& elem_index_from=entity_from.entity_2_elem(index_from)[0];
-		const Integer& iter_entity_from=entity_from.entity_2_elem(index_from)[1];
+		const auto& elem_index_from=entity_from.entity_2_elem(index_from)[0];
+		const auto& iter_entity_from=entity_from.entity_2_elem(index_from)[1];
 		const auto& nodes_from=mesh_.elem(elem_index_from).nodes;
 	
 	    // entity_nodes_from contains the local indexes of the nodes of the entity
@@ -81,10 +82,12 @@ inline std::vector<Integer> Connectivity<Dim,ManifoldDim,EntityDimFrom,SubEntity
 		
 		// loop on all the nodes of the entity with index=index_from
 		for(Integer nn=0;nn<EntityDimFrom + 1;nn++)
-			{const Integer& nn_from=entity_nodes_from[nn];
+			{
+			 const Integer& nn_from=entity_nodes_from[nn];
 			 // loop on all the elements surrounding
 			 for(Integer mm=0;mm<node2elem_[nn_from].size();mm++)
-				 el_neighs.push_back(node2elem_[nn_from][mm]);}
+				 el_neighs.push_back(node2elem_[nn_from][mm]);
+			}
 	
 		// define all the neighborhood elements in el_neighs
 		std::sort(el_neighs.begin(),el_neighs.end());
@@ -92,12 +95,14 @@ inline std::vector<Integer> Connectivity<Dim,ManifoldDim,EntityDimFrom,SubEntity
 	
 		// loop on all the surrounding elements
 		for(Integer elem_iter_to=0;elem_iter_to< el_neighs.size();elem_iter_to++)
-		   {const Integer &elem_index_to=el_neighs[elem_iter_to];
+		   {
+		    const auto &elem_index_to=el_neighs[elem_iter_to];
 		    const auto& nodes_to=mesh_.elem(elem_index_to).nodes;
 			// loop on all the entities of dimension EntityDimTo of the element with index=elem_index_to
-			for(Integer iter_entity_to=0;iter_entity_to< entity_to.entity_combinations();iter_entity_to++){
+			for(Integer iter_entity_to=0;iter_entity_to< entity_to.entity_combinations();iter_entity_to++)
+			    {
 				// consider the iter_entity nodes
-				const Integer& index_entity_to= entity_to.elem_2_entity(elem_index_to)[iter_entity_to];	
+				const auto& index_entity_to= entity_to.elem_2_entity(elem_index_to)[iter_entity_to];	
 				// entity_nodes_to contains the local indexes of the nodes of the simplex	
 				Combinations<ManifoldDim + 1, EntityDimTo+1>::generate(iter_entity_to,entity_nodes_to);
 				// now entity_nodes_to contains the global indexes of the nodes of the entity
@@ -111,35 +116,175 @@ inline std::vector<Integer> Connectivity<Dim,ManifoldDim,EntityDimFrom,SubEntity
 						 common_nodes++;
 				 
 				if(common_nodes >= SubEntityDimFrom + 1)
-				   FromTo.push_back(index_entity_to);}
+				   fromto.push_back(index_entity_to);
+				}
 			}
 		
-  		std::sort(FromTo.begin(),FromTo.end());
-		FromTo.erase( std::unique( FromTo.begin(), FromTo.end() ), FromTo.end() );	    
+  		std::sort(fromto.begin(),fromto.end());
+		fromto.erase( std::unique( fromto.begin(), fromto.end() ), fromto.end() );	    
 
-		return FromTo;}
+		return fromto;
+		}
 	};
 
 template <Integer Dim, Integer ManifoldDim>
 void Connectivity<Dim, ManifoldDim,0,0,ManifoldDim>::init(const Mesh<Dim,ManifoldDim> mesh)
 		{
-			const Integer n_nodes    = mesh.n_nodes();
-			const Integer n_elements = mesh.n_elements();			
+			const auto & n_nodes    = mesh.n_nodes();
+			const auto & n_elements = mesh.n_elements();			
 			val_.resize(n_nodes);
 				// loop on all the nodes of all the active elements 
 				// add to node_2_element the corresponding active element
-				for(Integer i = 0; i < n_elements; ++i) {
+				for(Integer i = 0; i < n_elements; ++i) 
+				   {
 						if(!mesh.is_active(i)) continue;
 
 						const auto &e    = mesh.elem(i);
 						// this can be put outside the loop, right?
-						const Integer nn = ManifoldDim + 1;
-						for(Integer k = 0; k < nn; ++k) {
-							val_[e.nodes[k]].push_back(i);
-									}
+						const auto & nn = ManifoldDim + 1;
+						for(Integer k = 0; k < nn; ++k) 
+						    {
+						    	val_[e.nodes[k]].push_back(i);
+							}
 					}		
 		};
 
+
+
+
+
+
+
+
+using std::cout;
+using std::endl;
+void connectivity_example()
+{
+
+    constexpr Integer ManifoldDim=4;
+	mars::Mesh<ManifoldDim, ManifoldDim> mesh;
+	read_mesh("../data/pentatope_1.MFEM", mesh);
+    //read_mesh("../data/cube_6.MFEM", mesh);
+	//read_mesh("../data/square_2_def.MFEM", mesh);
+    
+    
+    auto nelements=mesh.n_elements();
+//     for(auto i=0;i<nelements;i++)
+//     {
+// 		cout<<" i="<<i<<endl;
+// 		auto elem0=mesh.elem(i);
+// 		auto vertices=elem0.nodes;
+// 		auto nnodes=n_nodes(elem0);
+// 		auto nsides=n_sides(elem0);
+// 		auto ndims=n_dims(elem0);
+// 		auto dual_graph=mesh.dual_graph();
+// 	
+// 	
+// 		mesh.build_dual_graph();
+// 		cout<<" dual_graph.size()="<<dual_graph.size()<<endl;
+// 		for(int ii=0;ii<dual_graph.size();++ii)
+// 		{
+// 		auto adj=dual_graph.adj(ii);
+// 		cout<<"dual_graph.n_adjacients="<<dual_graph.n_adjacients(ii)<<endl;
+// 		for(int jj=0;jj<ManifoldDim+1;++jj)
+// 			{
+// 			cout<<" dual_graph="<<adj[jj]<<endl;
+// 			}
+// 		}
+// 	
+// 	
+// 		for (auto row = vertices.begin(); row != vertices.end(); row++) 
+// 				 {
+// 				 //  for (auto col = row->begin(); col != row->end(); col++) {
+// 				 // do stuff ...
+// 				 auto p=(mesh.point(*row)).values;
+// 				 for( auto point_it=p.begin(); point_it!=p.end();point_it++)
+// 					 cout<<" point("<<*row<<")="<<*point_it<<endl;
+// 					 //}
+// 				  }
+// 
+// 				//code here to print record wise.
+// 
+// 		cout<<"n_nodes="<<nnodes<<endl;
+// 		cout<<"n_sides="<<nsides<<endl;
+// 		cout<<"n_dims="<<ndims<<endl;
+//     }
+    
+
+
+
+    constexpr Integer entity_from_index=7;
+    constexpr Integer entitydim_from=2;
+    constexpr Integer subentitydim_from=1;
+    constexpr Integer entitydim_to=2;
+    
+    NodeToElem4	node2elem3(mesh);
+	auto node2elem=node2elem3.val();
+	
+	TriangleMap4 edge(mesh,node2elem);
+	//EdgeMap3 edge(mesh,node2elem);	
+	TriangleMap4 triangle(mesh,node2elem);	
+	//EdgeMap3 triangle(mesh,node2elem);	
+	Integer entityE[3];
+	Integer entityT[3];
+	Connectivity<ManifoldDim,ManifoldDim,entitydim_from,subentitydim_from,entitydim_to> connE2T(mesh,node2elem);
+		
+		
+		
+		
+		
+		
+    auto Eentity_2_elem=edge.entity_2_elem();
+    auto Eelem2entity=edge.elem_2_entity();
+		
+    auto Tentity_2_elem=triangle.entity_2_elem();
+    auto Telem2entity=triangle.elem_2_entity();
+    
+    cout<<"ENTITY FROM #elems="<<Tentity_2_elem.size()<<endl;
+    cout<<"ENTITY FROM dimension="<<entitydim_from<<endl;
+    cout<<"SUBENTITY FROM dimension="<<subentitydim_from<<endl;
+    cout<<"ENTITY TO dimension="<<entitydim_to<<endl;
+    cout<<"ENTITY FROM of interest id="<<entity_from_index<<endl;
+    
+//     for(int ii=entity_from_index;ii<Tentity_2_elem.size();ii++)
+//     {
+// 	
+// 		cout<<endl;
+// 		for(int jj=0;jj<1;jj++)
+// 		   cout<<Tentity_2_elem[ii][jj]<<" ";
+// 		cout<<"ENTITY FROM id="<<ii<<"   ";
+		const auto & elemii=mesh.elem(Tentity_2_elem[entity_from_index][0]);
+		Combinations<ManifoldDim + 1, triangle.num_of_points()>::generate(Tentity_2_elem[entity_from_index][1],entityT);
+		for(int jj=0;jj<triangle.num_of_points();jj++)
+		   cout<<elemii.nodes[entityT[jj]]<<" ";    
+//	}	
+
+
+
+
+    cout<<"ENTITY TO #elems="<<Eentity_2_elem.size()<<endl;
+    for(int ii=0;ii<Eentity_2_elem.size();ii++)
+    {
+    
+		cout<<endl;
+		for(int jj=0;jj<1;jj++)
+		   cout<<Eentity_2_elem[ii][jj]<<" ";
+		cout<<"--------ENTITY TO id="<<ii<<"   ";
+		const auto & elemii=mesh.elem(Eentity_2_elem[ii][0]);
+		Combinations<ManifoldDim + 1, edge.num_of_points()>::generate(Eentity_2_elem[ii][1],entityE);
+		for(int jj=0;jj<edge.num_of_points();jj++)
+		   cout<<elemii.nodes[entityE[jj]]<<" ";    
+	}	
+
+	auto connE82T=connE2T.compute(triangle,entity_from_index,edge);
+
+    cout<<endl;
+    for(Integer ii=0;ii<connE82T.size();ii++)
+       cout<<" CONNECTIONS: ="<<connE82T[ii]<<endl;
+       
+
+
+    }
 }
 
 #endif
--- mars_entity_impl.hpp
+++ mars_entity_impl.hpp
@@ -28,7 +28,17 @@ template<Integer Dim,Integer ManifoldDim,Integer EntityDim>
 void Entity<Dim,ManifoldDim,EntityDim>::init_entity(const Mesh<Dim,ManifoldDim> mesh,const std::vector< std::vector<Integer> > node_2_element,
                                     std::vector<std::array<Integer,2>> &entity_2_elem_, std::vector<std::array<Integer, entity_combinations() >> &elem_2_entity_)
  {
-    const Integer n_elements = mesh.n_elements();
+ 
+ 
+ 
+    static_assert(Dim>=0 , " the space dimension must be non negative ");
+    static_assert(ManifoldDim>=0 , " the manifold dimension must be non negative ");
+    static_assert(EntityDim>=0 , " the entity dimension must be non negative ");    
+    static_assert(Dim>=ManifoldDim , " the space dimension dimension must be greater than or equal to the manifold dimension ");
+    static_assert(ManifoldDim>=EntityDim , " the manifold dimension must be greater than or equal to the entity dimension ");
+
+
+    const auto& n_elements = mesh.n_elements();
     Integer entity_number=0;
     //std::array<Integer, EntityDim+1> tmp;
     std::array<Integer, 2> tmp;
@@ -38,11 +48,10 @@ void Entity<Dim,ManifoldDim,EntityDim>::init_entity(const Mesh<Dim,ManifoldDim> 
     std::array<Integer, EntityDim+1> entity_nodes1;
     std::array<Integer, EntityDim+1> entity_nodes2;
     std::array<Integer, EntityDim+1> entity_nodes3;
-    //std::vector<std::vector<Integer> > 
-    //elem_2_entity_(n_elements);
     elem_2_entity_.resize(n_elements);
     // loop on all the elements
-    for(Integer elem_iter1 = 0; elem_iter1 < n_elements; ++elem_iter1) {
+    for(Integer elem_iter1 = 0; elem_iter1 < n_elements; ++elem_iter1) 
+      {
 	
 		const auto &el1 = mesh.elem(elem_iter1);
 		const auto &el_nodes1=el1.nodes;
@@ -50,7 +59,8 @@ void Entity<Dim,ManifoldDim,EntityDim>::init_entity(const Mesh<Dim,ManifoldDim> 
 		// find all the elements touching the element=elem_iter1
 		for(Integer ii=0;ii<ManifoldDim+1;ii++)
 			   for(Integer jj=0;jj<node_2_element[el_nodes1[ii]].size();jj++)
-			   {el_neighs.push_back(node_2_element[el_nodes1[ii]][jj]);
+			   {
+			    el_neighs.push_back(node_2_element[el_nodes1[ii]][jj]);
 			   } 
  
 		// remove all the elements larger than  elem_iter1
@@ -82,7 +92,8 @@ void Entity<Dim,ManifoldDim,EntityDim>::init_entity(const Mesh<Dim,ManifoldDim> 
 			const auto &el_nodes2=el2.nodes;
 		
 			// loop on all the entity of the element e2
-			for(Integer iter_entity_e2=0;iter_entity_e2<entity_combinations();iter_entity_e2++){
+			for(Integer iter_entity_e2=0;iter_entity_e2<entity_combinations();iter_entity_e2++)
+			   {
 			
 				Combinations<ManifoldDim + 1, EntityDim+1>::generate(iter_entity_e2,entity_e2);
 			
@@ -91,9 +102,10 @@ void Entity<Dim,ManifoldDim,EntityDim>::init_entity(const Mesh<Dim,ManifoldDim> 
 						
 				std::sort(std::begin(entity_nodes2), std::end(entity_nodes2));  
 			
-				if (std::equal(std::begin(entity_nodes1), std::end(entity_nodes1), std::begin(entity_nodes2))){
+				if (std::equal(std::begin(entity_nodes1), std::end(entity_nodes1), std::begin(entity_nodes2)))
+				       {
 				       // all the entities already computed on elem=elem_iter2
-				       auto elem2entity=elem_2_entity_[elem_iter2];
+				       const auto &elem2entity=elem_2_entity_[elem_iter2];
 				       // loop on each of these, to find the one that equals iter_entity_e1
 				       for(auto i: elem2entity)
 				          {
@@ -108,18 +120,21 @@ void Entity<Dim,ManifoldDim,EntityDim>::init_entity(const Mesh<Dim,ManifoldDim> 
 				          if(std::equal(std::begin(entity_nodes3), std::end(entity_nodes3), std::begin(entity_nodes1)))
 				             {
 				             elem_2_entity_[elem_iter1][iter_entity_e1]=i;
-				             break;}
+				             break;
+				             }
 				          }
 					   found_entity=true;
-					   break;}         
+					   break;
+					   }         
 				}  
 			
 				if(found_entity==true)
 				   break;
 			
-				}
+			}
 		  // if no neighbborhood element share the same entity, then we must add this entity   
-		  if(found_entity==false){
+		  if(found_entity==false)
+		           {
 				   //std::copy(std::begin(entity_nodes1), std::end(entity_nodes1), std::begin(tmp));
 				   //entity_.push_back( tmp);
 				   tmp[0]=elem_iter1; 
