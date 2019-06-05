1095ecad7c4b18e53e1fcac51a3ee771a0e4cde2
Daniel Ganellari <gandanie@daint107.login.cscs.ch>
2019-06-05 16:48:10 +0200

    Kokkos 2D Mesh generation ready. Next step optimizations.

--- generation/mars_mesh_generation_kokkos.hpp
+++ generation/mars_mesh_generation_kokkos.hpp
@@ -85,164 +85,23 @@ bool generate_cube(ParallelMesh<Dim, ManifoldDim>& mesh, const Integer xDim,
 
 	using namespace Kokkos;
 
-
-	assert(ManifoldDim <= Dim);
 	assert(Dim <= 3);
+	assert(ManifoldDim <= Dim);
 
-	switch (ManifoldDim) {
+	mesh.generate_points(xDim,yDim);
 
-	case 1: {
-
-		assert(xDim != 0);
-		assert(yDim == 0);
-		assert(zDim == 0);
-
-
-		const int n_elements = xDim;
-		const int n_nodes = xDim + 1;
-		mesh.reserve(n_elements, n_nodes);
-
-		mesh.generate_points(n_nodes,xDim);
-		mesh.generate_elements(n_elements);
-
-		return true;
-	}
-
-	case 2: {
-
-		assert(xDim != 0);
-		assert(yDim != 0);
-		assert(zDim == 0);
-
-		const int n_elements = 2 * xDim * yDim;
-		const int n_nodes = (xDim + 1) * (yDim + 1);
-		mesh.reserve(n_elements, n_nodes);
-
-		mesh.generate_points_2D(xDim,yDim);
-		//Kokkos::fence();
-		mesh.generate_elements_2D(xDim,yDim);
-		//Kokkos::fence();
-
-
-		return true;
-	}
-	/*case 3: { //building the tetra mesh from the hex27 elem
-
-		assert(xDim != 0);
-		assert(yDim != 0);
-		assert(zDim != 0);
-
-		const int n_elements = xDim * yDim * zDim * 24; //24 tetrahedrons on one hex27
-		const int n_nodes = (2 * xDim + 1) * (2 * yDim + 1) * (2 * zDim + 1);
-
-		const int n_tetra_nodes = 5 * (xDim * yDim * zDim)
-				+ 2 * (xDim * yDim + xDim * zDim + yDim * zDim)
-				+ (xDim + yDim + zDim) + 1;
-
-		std::vector<bool> active_nodes(n_nodes);
-
-		// mesh points need resize because the point vector is getting accessed using the [] operator.
-		//First the center node is added somewhere at the end of the vector and then all the other nodes are added to it.
-		mesh.reserve_elements(n_elements);
-		mesh.resize_points(n_tetra_nodes);
-
-		std::array<Integer, hex_side_n_nodes> side;
-
-		int el_id = 1;
-		for (Integer k = 0; k < 2 * zDim; k += 2) {
-			for (Integer j = 0; j < 2 * yDim; j += 2) {
-				for (Integer i = 0; i < 2 * xDim; i += 2) {
-
-					//build the hex27 element which serves to generalize the idea to many hex27.
-					//without generating the hex27 element first there is no easy way to create the sides.
-					//Locally, using local element indexing it is possible.
-					std::array<Integer, hex_n_nodes> hex;
-					build_hex27(hex, xDim, yDim, i, j, k);
-
-					//add center of the hex to the new points.
-					int centerHex = n_nodes / 2 + el_id;
-
-					Vector<Real, Dim> p(
-							{ static_cast<Real>(i+1)
-									/ static_cast<Real>(2 * xDim),
-									static_cast<Real>(j+1)
-											/ static_cast<Real>(2 * yDim),
-									static_cast<Real>(k+1)
-											/ static_cast<Real>(2 * zDim), });
-
-					mesh.point(centerHex) = p;
-
-					//build tetrahedra elements from the hex27 faces.
-					for (unsigned int i = 0; i < hex_n_sides; ++i) {
-
-						//use the hex27 element local connectivity to build the tetrahedra connectivity
-						for (unsigned int j = 0; j < hex_side_n_nodes; ++j) {
-							side[j] = hex[hex_side_nodes[i][j]];
-						}
-
-						//build 4 tetrahedra out of one face.
-						for (unsigned int k = 0; k < 4; k++) {
-
-							auto& e = mesh.add_elem();
-
-							e.nodes[0] = side[k] / 2;
-							e.nodes[1] = side[8] / 2; // midface point always the last element.
-							e.nodes[2] = (k == 3 ? side[0] : side[k + 1]) / 2; // rotation to catch all combinations.
-							e.nodes[3] = centerHex; // the center of the cube.
-
-							active_nodes[side[k]] = true;
-							active_nodes[side[8]] = true;
-							active_nodes[(k == 3 ? side[0] : side[k + 1])] =
-									true;
-							//active_nodes[hex[26]]=true;
-						}
-
-					}
-					++el_id;
-				}
-			}
-		}
-
-		//first the element indices as above
-		//and then at this moment add the only the needed points to avoid extra node removal.
-		for (Integer k = 0; k <= 2 * zDim; ++k) {
-			for (Integer j = 0; j <= 2 * yDim; ++j) {
-				for (Integer i = 0; i <= 2 * xDim; ++i) {
-
-					Integer in = index(xDim, yDim, i, j, k);
-
-					if (active_nodes[in]) {
-
-						mesh.point(in / 2)[0] = static_cast<Real>(i)
-												/ static_cast<Real>(2 * xDim);
-						mesh.point(in / 2)[1] = static_cast<Real>(j)
-												/ static_cast<Real>(2 * xDim);
-						mesh.point(in / 2)[2] = static_cast<Real>(k)
-												/ static_cast<Real>(2 * xDim);
-					}
-
-				}
-			}
-		}
-
-		return true;
-	}*/
-	default: {
-
-		std::cerr << "Not implemented for other dimensions yet" << std::endl;
-		return false;
-	}
-	}
+	mesh.generate_elements(xDim,yDim);
 }
 
 bool generate_line(ParallelMesh<1, 1>& mesh, const Integer xDim) {
 	return generate_cube(mesh, xDim, 0, 0);
 }
 
-/*bool generate_square(Mesh2& mesh, const Integer xDim, const Integer yDim) {
+bool generate_square(ParallelMesh<2, 2>& mesh, const Integer xDim, const Integer yDim) {
 	return generate_cube(mesh, xDim, yDim, 0);
 }
 
+/*
 bool generate_point(Mesh<1, 0>& mesh) {
  return generate_cube(mesh,0,0,0);
  }
