68bdd9e52a6d23c1ebb5124892166bd39f773310
zulianp <patrick.zulian@gmail.com>
2020-07-20 13:19:18 +0200

    added example test

--- tests/mars_MeshGenerationTest.cpp
+++ tests/mars_MeshGenerationTest.cpp
@@ -1,10 +1,29 @@
-
 #include "gtest/gtest.h"
-#include "mars.hpp"
 
-namespace mars {
-    TEST(Mesh4, generate) {
-        Mesh4 m;
-        EXPECT_TRUE(m.n_elements() == 0);
+#include "mars.hpp"
+using namespace mars;
+
+template <typename MeshType>
+class MeshGenerationTest : public testing::Test {
+public:
+    void SetUp() override {
+        constexpr Integer n_x_dim = 10;
+        for (Integer d = 0; d < MeshType::Dim; ++d) {
+            dims[d] = n_x_dim;
+        }
     }
-}  // namespace mars
+
+protected:
+    std::array<Integer, 4> dims{0, 0, 0, 0};
+};
+
+using MeshGenerationTestTypes = ::testing::Types<Mesh2, Mesh3>;
+
+TYPED_TEST_SUITE(MeshGenerationTest, MeshGenerationTestTypes);
+
+TYPED_TEST(MeshGenerationTest, Generate) {
+    TypeParam mesh;
+    generate_cube(mesh, this->dims[0], this->dims[1], this->dims[2]);
+    mesh.build_dual_graph();
+    EXPECT_TRUE(mesh.check_side_ordering());
+}
