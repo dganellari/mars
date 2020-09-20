07497326e1ad6ec7ebd7e43293828b3a58effb29
zulianp <patrick.zulian@gmail.com>
2020-09-20 18:16:35 +0200

    fixed compilation issues on macos

--- cmake/FindMPIExtended.cmake
+++ cmake/FindMPIExtended.cmake
@@ -1,75 +1,57 @@
-#First with try with clang compile
-find_library(MPI_CLANG_LIBRARY  
-	NAMES mpi_cxx
-		  mpicxx-mpich-clang
-		  mpichcxx
-		  mpicxx
-	PATHS /opt/local/lib/openmpi-mp/
-		  /opt/local/lib/mpich-mp/
-		  /opt/local/lib/mpich-clang/
-		  /opt/local/lib
+# First with try with clang compile
+find_library(
+    MPI_CLANG_LIBRARY
+    NAMES mpi_cxx mpicxx-mpich-clang mpichcxx mpicxx
+    PATHS /opt/local/lib/openmpi-mp/ /opt/local/lib/mpich-mp/
+          /opt/local/lib/mpich-clang/ /opt/local/lib
+    DOC "The MPI_CLANG_LIBRARY library to link against")
 
-	DOC "The MPI_CLANG_LIBRARY library to link against"
-) 
+if(MPI_CLANG_LIBRARY)
 
+    set(MPI_CXX_LIBRARIES ${MPI_CLANG_LIBRARY})
 
-IF(MPI_CLANG_LIBRARY)
+    get_filename_component(MPI_LIB_DIR ${MPI_CLANG_LIBRARY} PATH)
 
-	SET(MPI_CXX_LIBRARIES ${MPI_CLANG_LIBRARY})
+    find_path(
+        MPI_CLANG_HEADERS mpi.h
+        HINTS ${MPI_LIB_DIR}/../../include
+              ${MPI_LIB_DIR}/../include
+              /opt/local/include/openmpi-mp/
+              /opt/local/include/mpich-mp
+              ${MPI_LIB_DIR}/../../include/openmpi-mp/
+              ${MPI_LIB_DIR}/../../include/mpich-clang
+              ${MPI_LIB_DIR}/../include/mpich-clang
+              /opt/local/include/mpich-clang
+        DOC "The MPI_CLANG_HEADERS path")
 
-	get_filename_component(MPI_LIB_DIR ${MPI_CLANG_LIBRARY} PATH)
-	
-	find_path(MPI_CLANG_HEADERS mpi.h
-		HINTS ${MPI_LIB_DIR}/../../include
-			  ${MPI_LIB_DIR}/../include
-				 /opt/local/include/openmpi-mp/
-				 /opt/local/include/mpich-mp
-				 ${MPI_LIB_DIR}/../../include/openmpi-mp/
-			 	 ${MPI_LIB_DIR}/../../include/mpich-clang
-				 ${MPI_LIB_DIR}/../include/mpich-clang
-				 /opt/local/include/mpich-clang
-		DOC "The MPI_CLANG_HEADERS path"
-	)	
+    if(MPI_CLANG_HEADERS)
+        find_file(
+            MPI_CXX_COMPILER
+            NAMES mpicxx mpicxx-openmpi-mp mpicxx-mpich-clang
+            HINTS ${MPI_CLANG_HEADERS}/../bin ${MPI_CLANG_HEADERS}/../../bin
+                  ${MPI_LIB_DIR}/../bin ${MPI_LIB_DIR}/../../bin /opt/local/bin/
+            DOC "the MPI_COMPILER_PATH dir")
 
-	IF(MPI_CLANG_HEADERS)
-		find_file(MPI_CXX_COMPILER 
-			NAMES mpicxx
-				  mpicxx-openmpi-mp 
-				  mpicxx-mpich-clang
-			HINTS ${MPI_CLANG_HEADERS}/../bin
-			 	  ${MPI_CLANG_HEADERS}/../../bin
-				  ${MPI_LIB_DIR}/../bin
-				  ${MPI_LIB_DIR}/../../bin
-				  /opt/local/bin/
-			DOC "the MPI_COMPILER_PATH dir"
+        find_file(
+            MPI_C_COMPILER
+            NAMES mpicc mpicc-openmpi-mp mpicc-mpich-clang
+            HINTS ${MPI_CLANG_HEADERS}/../bin ${MPI_CLANG_HEADERS}/../../bin
+                  ${MPI_LIB_DIR}/../bin ${MPI_LIB_DIR}/../../bin /opt/local/bin/
+            DOC "the MPI_COMPILER_PATH dir")
 
-		)
+        if(MPI_CXX_COMPILER AND MPI_C_COMPILER)
+            # set variables
+            set(MPI_FOUND TRUE)
+            set(MPI_CXX_LIBRARIES ${MPI_CLANG_LIBRARY})
+            set(MPI_CXX_INCLUDE_PATH ${MPI_CLANG_HEADERS})
+            include_directories(${MPI_CLANG_HEADERS})
+        endif()
+    endif()
+endif()
 
-		find_file(MPI_C_COMPILER 
-			NAMES mpicc
-			      mpicc-openmpi-mp
-				  mpicc-mpich-clang
-			HINTS ${MPI_CLANG_HEADERS}/../bin
-			 	  ${MPI_CLANG_HEADERS}/../../bin
-				  ${MPI_LIB_DIR}/../bin
-				  ${MPI_LIB_DIR}/../../bin
-				  /opt/local/bin/
-			DOC "the MPI_COMPILER_PATH dir"
+# MESSAGE(STATUS "${MPI_CLANG_LIBRARY} ${MPI_CLANG_HEADERS}
+# ${MPI_CXX_COMPILER}")
 
-		)
-
-		IF(MPI_CXX_COMPILER AND MPI_C_COMPILER)
-			#set variables
-			SET(MPI_FOUND TRUE)
-			SET(MPI_CXX_LIBRARIES ${MPI_CLANG_LIBRARY})
-			SET(MPI_CXX_INCLUDE_PATH ${MPI_CLANG_HEADERS})
-		ENDIF()
-	ENDIF()
-ENDIF()
-
-# MESSAGE(STATUS "${MPI_CLANG_LIBRARY} ${MPI_CLANG_HEADERS} ${MPI_CXX_COMPILER}")
-
-
-IF(NOT MPI_FOUND)
-find_package(MPI)
-ENDIF()
+if(NOT MPI_FOUND)
+    find_package(MPI)
+endif()
--- examples/mars_poisson.hpp
+++ examples/mars_poisson.hpp
@@ -39,7 +39,7 @@ u = P uk */
 #include "Kokkos_Parallel_Reduce.hpp"
 #include "mars_context.hpp"
 #include "mars_globals.hpp"
-#include <bits/c++config.h>
+// x#include <bits/c++config.h>
 #include <exception>
 #include <iostream>
 #include <tuple>
@@ -53,11 +53,10 @@ u = P uk */
 #ifdef WITH_KOKKOS
 #include "mars_distributed_data_management.hpp"
 #include "mars_distributed_mesh_generation.hpp"
-#endif //WITH_KOKKOS
+#endif // WITH_KOKKOS
 #endif
 
-namespace mars
-{
+namespace mars {
 
 using DMQ2 = DM<DistributedQuad4Mesh, 2, double, double>;
 /*
@@ -68,224 +67,218 @@ enum class DMDataDesc
 };
  */
 
-template <Integer idx>
-using DMDataType = typename DMQ2::UserDataType<idx>;
+template <Integer idx> using DMDataType = typename DMQ2::UserDataType<idx>;
 
-template <typename... T>
-using tuple = mars::ViewsTuple<T...>;
+template <typename... T> using tuple = mars::ViewsTuple<T...>;
 
 using dm_tuple = typename DMQ2::user_tuple;
 
-template<Integer Type>
-void print_dof(const SFC<Type> &dof, const int rank)
-{
-    Kokkos::parallel_for("for", dof.get_elem_size(), MARS_LAMBDA(const int i) {
+template <Integer Type> void print_dof(const SFC<Type> &dof, const int rank) {
+  Kokkos::parallel_for(
+      "for", dof.get_elem_size(), MARS_LAMBDA(const int i) {
         Integer sfc_elem = dof.get_view_elements()(i);
 
         double point[3];
-        get_vertex_coordinates_from_sfc<Type>(sfc_elem, point, dof.get_XDim(), dof.get_YDim(), dof.get_ZDim());
+        get_vertex_coordinates_from_sfc<Type>(sfc_elem, point, dof.get_XDim(),
+                                              dof.get_YDim(), dof.get_ZDim());
 
-        printf("dof: %li - (%lf, %lf) - rank: %i\n", i, point[0], point[1], rank);
-    });
+        printf("dof: %li - (%lf, %lf) - rank: %i\n", i, point[0], point[1],
+               rank);
+      });
 }
 
-template<Integer Type>
-void print_dofs(const DMQ2 &dm, const int rank)
-{
-    SFC<Type> dof = dm.get_local_dof_enum();
-    Kokkos::parallel_for("for", dof.get_elem_size(), MARS_LAMBDA(const int i) {
+template <Integer Type> void print_dofs(const DMQ2 &dm, const int rank) {
+  SFC<Type> dof = dm.get_local_dof_enum();
+  Kokkos::parallel_for(
+      "for", dof.get_elem_size(), MARS_LAMBDA(const int i) {
         const Integer sfc_elem = dm.local_to_sfc(i);
         const Integer global_dof = dm.local_to_global(i);
 
         double point[3];
-        get_vertex_coordinates_from_sfc<Type>(sfc_elem, point, dof.get_XDim(), dof.get_YDim(), dof.get_ZDim());
+        get_vertex_coordinates_from_sfc<Type>(sfc_elem, point, dof.get_XDim(),
+                                              dof.get_YDim(), dof.get_ZDim());
 
-        printf("dof: %li - gdof: %li --- (%lf, %lf) - rank: %i\n", i, global_dof, point[0], point[1], rank);
-    });
+        printf("dof: %li - gdof: %li --- (%lf, %lf) - rank: %i\n", i,
+               global_dof, point[0], point[1], rank);
+      });
 }
 
-//print thlocal and the global number of the dof within each element.
-//the dof enumeration within eachlement is topological
-void print_elem_global_dof(const DMQ2 dm, const int rank)
-{
-    dm.elem_iterate(MARS_LAMBDA(const Integer elem_index) {
-        //go through all the dofs of the elem_index element
-        for (int i = 0; i < DMQ2::elem_nodes; i++)
-        {
-            //get the local dof of the i-th index within thelement
-            const Integer local_dof = dm.get_elem_local_dof(elem_index, i);
-            //convert the local dof number to global dof number
-            Dof d = dm.local_to_global_dof(local_dof);
+// print thlocal and the global number of the dof within each element.
+// the dof enumeration within eachlement is topological
+void print_elem_global_dof(const DMQ2 dm, const int rank) {
+  dm.elem_iterate(MARS_LAMBDA(const Integer elem_index) {
+    // go through all the dofs of the elem_index element
+    for (int i = 0; i < DMQ2::elem_nodes; i++) {
+      // get the local dof of the i-th index within thelement
+      const Integer local_dof = dm.get_elem_local_dof(elem_index, i);
+      // convert the local dof number to global dof number
+      Dof d = dm.local_to_global_dof(local_dof);
 
-            //do something. In this case we are printing.
-            printf("lgm: local: %li, global: %li, proc: %i, rank:%i\n",  local_dof, d.get_gid(), d.get_proc(), rank);
+      // do something. In this case we are printing.
+      printf("lgm: local: %li, global: %li, proc: %i, rank:%i\n", local_dof,
+             d.get_gid(), d.get_proc(), rank);
+    }
+  });
+}
+
+// print the local and global numbering of the ghost dofs per process
+void print_ghost_dofs(const DMQ2 dm, const int rank) {
+  Kokkos::parallel_for(
+      dm.get_ghost_lg_map().capacity(), KOKKOS_LAMBDA(Integer i) {
+        if (dm.get_ghost_lg_map().valid_at(i)) {
+          auto sfc = dm.get_ghost_lg_map().key_at(i);
+          auto global_dof = dm.get_ghost_lg_map().value_at(i);
+          printf("local: %li, global: %li - proc: %li - rank: %li\n",
+                 dm.sfc_to_local(sfc), global_dof.get_gid(),
+                 global_dof.get_proc(), rank);
         }
-    });
+      });
 }
 
-//print the local and global numbering of the ghost dofs per process
-void print_ghost_dofs(const DMQ2 dm, const int rank)
-{
-    Kokkos::parallel_for(
-        dm.get_ghost_lg_map().capacity(), KOKKOS_LAMBDA(Integer i) {
-            if (dm.get_ghost_lg_map().valid_at(i))
-            {
-                auto sfc = dm.get_ghost_lg_map().key_at(i);
-                auto global_dof = dm.get_ghost_lg_map().value_at(i);
-                printf("local: %li, global: %li - proc: %li - rank: %li\n", dm.sfc_to_local(sfc), global_dof.get_gid(), global_dof.get_proc(), rank);
-            }
-        });
-}
+// form the matrix free operator
+template <Integer u, Integer v>
+void form_operator(const DMQ2 dm, const int rank) {
+  // go through each element and for each element iterate through its dofs
+  dm.elem_iterate(MARS_LAMBDA(const Integer elem_index) {
+    for (int i = 0; i < DMQ2::elem_nodes; i++) {
+      // forach dof get the local number
+      const Integer local_dof = dm.get_elem_local_dof(elem_index, i);
 
-//form the matrix free operator
-template<Integer u, Integer v>
-void form_operator(const DMQ2 dm, const int rank)
-{
-    //go through each element and for each element iterate through its dofs
-    dm.elem_iterate(MARS_LAMBDA(const Integer elem_index) {
-        for (int i = 0; i < DMQ2::elem_nodes; i++)
-        {
-            //forach dof get the local number
-            const Integer local_dof = dm.get_elem_local_dof(elem_index, i);
+      // use the local number to read the corresponding user data
+      double ui = dm.get_dof_data<v>(local_dof) + 1;
+      // TODO: apply the operator
 
-            //use the local number to read the corresponding user data
-            double ui = dm.get_dof_data<v>(local_dof) + 1;
-            //TODO: apply the operator
-
-            //atomically updated the contributions to the same dof
-            Kokkos::atomic_add(&dm.get_dof_data<u>(local_dof), ui);
-        }
-    });
+      // atomically updated the contributions to the same dof
+      Kokkos::atomic_add(&dm.get_dof_data<u>(local_dof), ui);
+    }
+  });
 }
 
 template <Integer... dataidx>
-void scatter_add_ghost_data(DMQ2 &dm, const context &context)
-{
-    //scatter the data to the procs and keep them in a boundary data tuple
-    //again if no template argument is specified all the data is scattered.
-    //if not all of them then be careful since the tuple is not initialized on the others
-    //example: dm_tuple boundary_data = dm.scatter_ghost_data<1>(context);
-    dm_tuple boundary_data = dm.scatter_ghost_data<dataidx...>(context);
+void scatter_add_ghost_data(DMQ2 &dm, const context &context) {
+  // scatter the data to the procs and keep them in a boundary data tuple
+  // again if no template argument is specified all the data is scattered.
+  // if not all of them then be careful since the tuple is not initialized on
+  // the others example: dm_tuple boundary_data =
+  // dm.scatter_ghost_data<1>(context);
+  dm_tuple boundary_data = dm.scatter_ghost_data<dataidx...>(context);
 
-    //use the scattered data "boundary_data" to do ops like max, add or min in the dof contributions.
-    //Otherwise you can use predifined features like scatter_add as following.
-    //careful to use the same template argument for specifing the data as in the scatter_ghost_data
-    //since otherwise you might try to access uninitialized tuplelement and get seg faults.
-    //example:: dm.scatter_add<1>(boundary_data); If: dm.scatter_add<0>(boundary_data) then seg faults.
-    dm.scatter_add<dataidx...>(boundary_data);
-    /* dm.scatter_max<u>(boundary_data); */
-    /* dm.scatter_min<u>(boundary_data); */
+  // use the scattered data "boundary_data" to do ops like max, add or min in
+  // the dof contributions. Otherwise you can use predifined features like
+  // scatter_add as following. careful to use the same template argument for
+  // specifing the data as in the scatter_ghost_data since otherwise you might
+  // try to access uninitialized tuplelement and get seg faults. example::
+  // dm.scatter_add<1>(boundary_data); If: dm.scatter_add<0>(boundary_data) then
+  // seg faults.
+  dm.scatter_add<dataidx...>(boundary_data);
+  /* dm.scatter_max<u>(boundary_data); */
+  /* dm.scatter_min<u>(boundary_data); */
 }
 
-void poisson(int &argc, char **&argv, const int level)
-{
+void poisson(int &argc, char **&argv, const int level) {
 
-    using namespace mars;
-    try
+  using namespace mars;
+  try {
+    mars::proc_allocation resources;
+    /*
+    // try to detect how many threads can be run on this system
+    resources.num_threads = marsenv::thread_concurrency();
+
+    // override thread count if the user set MARS_NUM_THREADS
+    if (auto nt = marsenv::get_env_num_threads())
     {
-        mars::proc_allocation resources;
-        /*
-        // try to detect how many threads can be run on this system
-        resources.num_threads = marsenv::thread_concurrency();
-
-        // override thread count if the user set MARS_NUM_THREADS
-        if (auto nt = marsenv::get_env_num_threads())
-        {
-            resources.num_threads = nt;
-        } */
+        resources.num_threads = nt;
+    } */
 
 #ifdef WITH_MPI
-        // initialize MPI
-        marsenv::mpi_guard guard(argc, argv, false);
+    // initialize MPI
+    marsenv::mpi_guard guard(argc, argv, false);
 
-        // assign a unique gpu to this rank if available
-        /*  resources.gpu_id = marsenv::find_private_gpu(MPI_COMM_WORLD); */
+    // assign a unique gpu to this rank if available
+    /*  resources.gpu_id = marsenv::find_private_gpu(MPI_COMM_WORLD); */
 
-        // create a distributed context
-        auto context = mars::make_context(resources, MPI_COMM_WORLD);
-        int proc_num = mars::rank(context);
+    // create a distributed context
+    auto context = mars::make_context(resources, MPI_COMM_WORLD);
+    int proc_num = mars::rank(context);
 #else
-        // resources.gpu_id = marsenv::default_gpu();
+    // resources.gpu_id = marsenv::default_gpu();
 
-        // // create a local context
-        // auto context = mars::make_context(resources);
+    // // create a local context
+    // auto context = mars::make_context(resources);
 #endif
 
 #ifdef WITH_KOKKOS
 
-        Kokkos::Timer timer;
-        //create the quad mesh distributed through the mpi procs.
-        DistributedQuad4Mesh mesh;
-        generate_distributed_cube(context, mesh, level, level, 0);
-        /* mesh.set_periodic(); //set the domain to be periodic */
-/*
-        const Integer xDim = mesh.get_XDim();
-        const Integer yDim = mesh.get_YDim();
-        const Integer zDim = mesh.get_ZDim();
- */
-        constexpr Integer Dim = DistributedQuad4Mesh::Dim;
+    Kokkos::Timer timer;
+    // create the quad mesh distributed through the mpi procs.
+    DistributedQuad4Mesh mesh;
+    generate_distributed_cube(context, mesh, level, level, 0);
+    /* mesh.set_periodic(); //set the domain to be periodic */
+    /*
+            const Integer xDim = mesh.get_XDim();
+            const Integer yDim = mesh.get_YDim();
+            const Integer zDim = mesh.get_ZDim();
+     */
+    constexpr Integer Dim = DistributedQuad4Mesh::Dim;
 
-        using Elem = typename DistributedQuad4Mesh::Elem;
-        //the type of the mesh elements. In this case quad4 (Type=4)
-        constexpr Integer Type = Elem::ElemType;
+    using Elem = typename DistributedQuad4Mesh::Elem;
+    // the type of the mesh elements. In this case quad4 (Type=4)
+    constexpr Integer Type = Elem::ElemType;
 
-        //create the dm object
-        DMQ2 dm(&mesh, context);
-        //enumerate the dofs locally and globally. The ghost dofs structures
-        //are now created and ready to use for the gather and scatter ops.
-        dm.enumerate_dofs(context);
-        //print locally owned dof numbering
-        /* print_dof<Type>(dm.get_global_dof_enum(), proc_num); */
+    // create the dm object
+    DMQ2 dm(&mesh, context);
+    // enumerate the dofs locally and globally. The ghost dofs structures
+    // are now created and ready to use for the gather and scatter ops.
+    dm.enumerate_dofs(context);
+    // print locally owned dof numbering
+    /* print_dof<Type>(dm.get_global_dof_enum(), proc_num); */
 
-        //print local dof numbering
-        /* print_dof<Type>(dm.get_local_dof_enum(), proc_num); */
-        /* print_dofs<Type>(dm, proc_num); */
+    // print local dof numbering
+    /* print_dof<Type>(dm.get_local_dof_enum(), proc_num); */
+    /* print_dofs<Type>(dm, proc_num); */
 
-        //print the global dofs for each element's local dof
-        /* print_elem_global_dof(dm, proc_num); */
+    // print the global dofs for each element's local dof
+    /* print_elem_global_dof(dm, proc_num); */
 
-        //use as more readable tuple index to identify the data
-        constexpr int u = 0;
-        constexpr int v = 1;
-        //local dof enum size
-        const Integer dof_size = dm.get_dof_size();
+    // use as more readable tuple index to identify the data
+    constexpr int u = 0;
+    constexpr int v = 1;
+    // local dof enum size
+    const Integer dof_size = dm.get_dof_size();
 
-        //initialize the values by iterating through local dofs
-        Kokkos::parallel_for("initdatavalues", dof_size,
-            MARS_LAMBDA(const Integer i) {
-                dm.get_dof_data<u>(i) = 0;
-                dm.get_dof_data<v>(i) = i;
-            });
+    // initialize the values by iterating through local dofs
+    Kokkos::parallel_for(
+        "initdatavalues", dof_size, MARS_LAMBDA(const Integer i) {
+          dm.get_dof_data<u>(i) = 0;
+          dm.get_dof_data<v>(i) = i;
+        });
 
-        //specify the tuple indices of the tuplelements that are needed to gather.
-        //if no index specified it gathers all views of the tuple. All data.
-        dm.gather_ghost_data<v>(context);
+    // specify the tuple indices of the tuplelements that are needed to gather.
+    // if no index specified it gathers all views of the tuple. All data.
+    dm.gather_ghost_data<v>(context);
 
-        form_operator<u, v>(dm, proc_num);
+    form_operator<u, v>(dm, proc_num);
 
-        //iterate through the local dofs and print the local number and the data
-        /* dm.dof_iterate(
-            MARS_LAMBDA(const Integer i) {
-                printf("lid: %li, u: %lf, v: %lf, rank: %i\n", i,
-                       dm.get_dof_data<u>(i), dm.get_dof_data<v>(i), proc_num);
-            }); */
+    // iterate through the local dofs and print the local number and the data
+    /* dm.dof_iterate(
+        MARS_LAMBDA(const Integer i) {
+            printf("lid: %li, u: %lf, v: %lf, rank: %i\n", i,
+                   dm.get_dof_data<u>(i), dm.get_dof_data<v>(i), proc_num);
+        }); */
 
-        scatter_add_ghost_data<u>(dm, context);
+    scatter_add_ghost_data<u>(dm, context);
 
-        dm.dof_iterate(
-            MARS_LAMBDA(const Integer i) {
-                printf("ggid: %li, u: %lf, v: %lf, rank: %i\n", i,
-                       dm.get_dof_data<u>(i), dm.get_dof_data<v>(i), proc_num);
-            });
+    dm.dof_iterate(MARS_LAMBDA(const Integer i) {
+      printf("ggid: %li, u: %lf, v: %lf, rank: %i\n", i, dm.get_dof_data<u>(i),
+             dm.get_dof_data<v>(i), proc_num);
+    });
 
-        double time = timer.seconds();
-        std::cout << "Matrix free took: " << time << " seconds." << std::endl;
+    double time = timer.seconds();
+    std::cout << "Matrix free took: " << time << " seconds." << std::endl;
 
 #endif
-    }
-    catch (std::exception &e)
-    {
-        std::cerr << "exception caught in ring miniapp: " << e.what() << "\n";
-    }
+  } catch (std::exception &e) {
+    std::cerr << "exception caught in ring miniapp: " << e.what() << "\n";
+  }
 }
 } // namespace mars
