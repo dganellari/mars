734abea1bdcbee2ce824a84106c6850534759309
zulianp <patrick.zulian@gmail.com>
2020-09-22 17:07:49 +0200

    added optional vtk dependency

---
+++ io/mars_io.cpp
@@ -0,0 +1,13 @@
+#include <vtkActor.h>
+#include <vtkCellArray.h>
+#include <vtkDataSetMapper.h>
+#include <vtkPointData.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+#include <vtkTetra.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkVertexGlyphFilter.h>
+#include <vtkXMLUnstructuredGridReader.h>
+#include <vtkXMLUnstructuredGridWriter.h>
