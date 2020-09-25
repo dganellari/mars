aeb822a79f1c805406317533f2c17637fe2aaadd
zulianp <patrick.zulian@gmail.com>
2020-09-25 17:21:27 +0200

    ...

--- examples/mars_st_example.cpp
+++ examples/mars_st_example.cpp
@@ -129,9 +129,6 @@ namespace mars {
         MARS_INLINE_FUNCTION Real det2(const Real *m) { return m[0] * m[3] - m[2] * m[1]; }
 
         MARS_INLINE_FUNCTION Real det3(const Real *m) {
-            // return m[0] * m[4] * m[8] + m[1] * m[5] * m[6] + m[2] * m[3] * m[4] - m[0] * m[5] * m[4] -
-            //        m[1] * m[3] * m[8] - m[2] * m[4] * m[6];
-
             return m[0] * m[4] * m[8] + m[1] * m[5] * m[6] + m[2] * m[3] * m[7] - m[0] * m[5] * m[7] -
                    m[1] * m[3] * m[8] - m[2] * m[4] * m[6];
         }
