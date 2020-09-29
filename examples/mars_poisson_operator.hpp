4af9454c9008fa7e1b07a888ffb2d49c02a496ce
zulianp <patrick.zulian@gmail.com>
2020-09-29 16:07:37 +0200

    ...

--- examples/mars_poisson_operator.hpp
+++ examples/mars_poisson_operator.hpp
@@ -6,16 +6,20 @@
 
 namespace mars {
 
-class PoissonOperator final : public Operator {
-public:
-  using DMType = mars::DMQ2;
+    class PoissonOperator final : public Operator {
+    public:
+        using DMType = mars::DMQ2;
 
-  PoissonOperator(DMType &dm, const context &c) : Operator(c) {}
+        PoissonOperator(DMType &dm, const context &c) : Operator(c) {}
 
-  void apply(const ViewVectorType<Real> &x,
-             ViewVectorType<Real> &op_x) override;
-};
+        void apply(const ViewVectorType<Real> &x, ViewVectorType<Real> &op_x) override;
+    };
 
-} // namespace mars
+    class CopyOperator final {
+    public:
+        void apply(const ViewVectorType<Real> &x, ViewVectorType<Real> &op_x) { Kokkos::deep_copy(op_x, x); }
+    };
+
+}  // namespace mars
 
 #endif
