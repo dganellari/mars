45bd3f1bfb4d998aac4f3312208ba33c56771dd7
liudmilakaragyaur <karagl@usi.ch>
2020-09-30 13:52:02 +0200

    script

--- run3D_timing.sh
+++ run3D_timing.sh
@@ -4,5 +4,5 @@ SP_PATH=/Users/liudmilakaragyaur/Documents/Eurohack2020_local/mars/build/st_exam
 
 for level in {5,10,20,30,40,50}
 do
-   $SP_PATH $level 0
+   srun -n4 --ntasks-per-core=1 --ntasks-per-node=1 --cpu-bind=no $SP_PATH $level 0
 done 
