78cbaa71716041b73a746588e71166af8692c196
liudmilakaragyaur <karagl@usi.ch>
2020-09-30 13:42:45 +0200

    exec time testing

--- examples/mars_st_example.cpp
+++ examples/mars_st_example.cpp
@@ -316,8 +316,9 @@ int main(int argc, char *argv[]) {
         // Example2Analitcal>().run(
         //     argc, argv);
 
-        ModelTest<ParallelMesh2, UMeshLaplace<ParallelMesh2>, Example3Dirichlet, Example3RHS, Example3Analitcal>().run(
-            argc, argv);
+        // ModelTest<ParallelMesh2, UMeshLaplace<ParallelMesh2>, Example3Dirichlet, Example3RHS,
+        // Example3Analitcal>().run(
+        //     argc, argv);
 
         // ModelTest<ParallelMesh2, UMeshSTHeatEquation<ParallelMesh2>, ST2BC<ParallelMesh2>, ST2RHS,
         // ST2Analitcal>().run(
@@ -335,9 +336,8 @@ int main(int argc, char *argv[]) {
         //     .run(argc, argv);
 
         // 3D example 1
-        // ModelTest<ParallelMesh3, UMeshSTHeatEquation<ParallelMesh3>, ST3D1BC<ParallelMesh3>, ST3D1RHS,
-        // ST3D1Analitcal>()
-        //     .run(argc, argv);
+        ModelTest<ParallelMesh3, UMeshSTHeatEquation<ParallelMesh3>, ST3D1BC<ParallelMesh3>, ST3D1RHS, ST3D1Analitcal>()
+            .run(argc, argv);
 
         // 3D example 2
         // ModelTest<ParallelMesh3, UMeshSTHeatEquation<ParallelMesh3>, ST3D2BC<ParallelMesh3>, ST3D2RHS,
---
+++ run3D_timing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SP_PATH=/Users/liudmilakaragyaur/Documents/Eurohack2020_local/mars/build/st_example
+
+for level in {5,10,20,30,40,50}
+do
+   $SP_PATH $level 0
+done 
