164fda80eecba108c8a4cc650d0756a3e86dd317
zulianp <patrick.zulian@gmail.com>
2020-10-02 11:40:55 +0200

    added script folder

--- run3D_timing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SP_PATH=/Users/liudmilakaragyaur/Documents/Eurohack2020_local/mars/build/st_example
-
-for level in {5,10,20,30,40,50}
-do
-   srun -n4 --ntasks-per-core=1 --ntasks-per-node=1 --cpu-bind=no $SP_PATH $level 0
-done 
