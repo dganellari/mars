fc96f352f78b9192c3884427ff8f6f3a31c5da43
zulianp <patrick.zulian@gmail.com>
2020-10-06 09:49:04 +0200

    fixed bad MPI flags

--- distributed/communication/mars_execution_context.cpp
+++ distributed/communication/mars_execution_context.cpp
@@ -27,7 +27,6 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
-
 #include <iostream>
 #include <memory>
 
@@ -38,58 +37,61 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 #include "mars_execution_context.hpp"
 //#include "mars_threading.hpp"
 
-#ifdef MARS_HAVE_MPI
+#ifdef WITH_MPI
 #include <mpi.h>
 #endif
 
 namespace mars {
 
 void execution_context_deleter::operator()(execution_context* p) const {
-    delete p;
+  delete p;
 }
 
-execution_context::execution_context(const proc_allocation& resources):
-    distributed(make_local_context())/* ,
-    thread_pool(std::make_shared<threading::task_system>(resources.num_threads)),
-    gpu(resources.has_gpu()? std::make_shared<gpu_context>(resources.gpu_id)
-                           : std::make_shared<gpu_context>()) */
+execution_context::execution_context(const proc_allocation& resources)
+    : distributed(
+          make_local_context()) /* ,
+thread_pool(std::make_shared<threading::task_system>(resources.num_threads)),
+gpu(resources.has_gpu()? std::make_shared<gpu_context>(resources.gpu_id)
+                      : std::make_shared<gpu_context>()) */
 {}
 
 context make_context(const proc_allocation& p) {
-    return context(new execution_context(p));
+  return context(new execution_context(p));
 }
 
-#ifdef MARS_HAVE_MPI
+#ifdef WITH_MPI
 template <>
-execution_context::execution_context(const proc_allocation& resources, MPI_Comm comm):
-    distributed(make_mpi_context(comm))/* ,
-    thread_pool(std::make_shared<threading::task_system>(resources.num_threads)),
-    gpu(resources.has_gpu()? std::make_shared<gpu_context>(resources.gpu_id)
-                           : std::make_shared<gpu_context>()) */
+execution_context::execution_context(const proc_allocation& resources,
+                                     MPI_Comm comm)
+    : distributed(
+          make_mpi_context(comm)) /* ,
+thread_pool(std::make_shared<threading::task_system>(resources.num_threads)),
+gpu(resources.has_gpu()? std::make_shared<gpu_context>(resources.gpu_id)
+                      : std::make_shared<gpu_context>()) */
 {}
 
 template <>
 context make_context<MPI_Comm>(const proc_allocation& p, MPI_Comm comm) {
-    return context(new execution_context(p, comm));
+  return context(new execution_context(p, comm));
 }
 #endif
 template <>
-execution_context::execution_context(
-        const proc_allocation& resources,
-        dry_run_info d):
-        distributed(make_dry_run_context(d.num_ranks, d.num_cells_per_rank))/* ,
-        thread_pool(std::make_shared<threading::task_system>(resources.num_threads)),
-        gpu(resources.has_gpu()? std::make_shared<gpu_context>(resources.gpu_id)
-                               : std::make_shared<gpu_context>()) */
+execution_context::execution_context(const proc_allocation& resources,
+                                     dry_run_info d)
+    : distributed(
+          make_dry_run_context(d.num_ranks, d.num_cells_per_rank)) /* ,
+thread_pool(std::make_shared<threading::task_system>(resources.num_threads)),
+gpu(resources.has_gpu()? std::make_shared<gpu_context>(resources.gpu_id)
+                      : std::make_shared<gpu_context>()) */
 {}
 
 template <>
 context make_context(const proc_allocation& p, dry_run_info d) {
-    return context(new execution_context(p, d));
+  return context(new execution_context(p, d));
 }
 
 std::string distribution_type(const context& ctx) {
-    return ctx->distributed->name();
+  return ctx->distributed->name();
 }
 
 /* bool has_gpu(const context& ctx) {
@@ -100,17 +102,10 @@ unsigned num_threads(const context& ctx) {
     return ctx->thread_pool->get_num_threads();
 } */
 
-unsigned num_ranks(const context& ctx) {
-    return ctx->distributed->size();
-}
+unsigned num_ranks(const context& ctx) { return ctx->distributed->size(); }
 
-unsigned rank(const context& ctx) {
-    return ctx->distributed->id();
-}
+unsigned rank(const context& ctx) { return ctx->distributed->id(); }
 
-bool has_mpi(const context& ctx) {
-    return ctx->distributed->name() == "MPI";
-}
+bool has_mpi(const context& ctx) { return ctx->distributed->name() == "MPI"; }
 
-} // namespace mars
-
+}  // namespace mars
