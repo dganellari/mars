3898aa98b97c7c6b49ed39003dfbf75d9ba24f7f
Daniel Ganellari <gandanie@daint102.login.cscs.ch>
2020-11-16 16:10:50 +0100

    More code optimizations related to the serial compilation of Mars. Solved the ParallelEdgeSelect issue.

--- base/mars_edge_select.hpp
+++ base/mars_edge_select.hpp
@@ -11,97 +11,61 @@
 
 namespace mars {
 
-	template<class Mesh>
-	class EdgeSelect {
-	public:
-		static const Integer Dim = Mesh::Dim;
-		static const Integer ManifoldDim = Mesh::ManifoldDim;
+    template <class Mesh, class Implementation_>
+    class EdgeSelect {
+    public:
+        static const Integer Dim = Mesh::Dim;
+        static const Integer ManifoldDim = Mesh::ManifoldDim;
 
-		virtual ~EdgeSelect() {}
-		virtual Integer select(
-			const Mesh &mesh,
-			const Integer element_id) const
-		{
-			//first edge selected
-			return 0;
-		}
+        virtual ~EdgeSelect() {}
+        virtual Integer select(const Mesh &mesh, const Integer element_id) const {
+            // first edge selected
+            return 0;
+        }
 
-		virtual Integer select(
-			const Mesh &mesh,
-			const Edge &neighbor_edge,
-			const Integer element_id) const
-		{
-			//first edge selected
-			return 0;
-		}
+        virtual Integer select(const Mesh &mesh, const Edge &neighbor_edge, const Integer element_id) const {
+            // first edge selected
+            return 0;
+        }
 
-		virtual Integer select(
-			const Mesh &mesh,
-			const Integer element_id,
-			const EdgeElementMap &edge_element_map) const
-		{
-			//first edge selected
-			return 0;
-		}
+        virtual Integer select(const Mesh &mesh,
+                               const Integer element_id,
+                               const EdgeElementMap &edge_element_map) const {
+            // first edge selected
+            return 0;
+        }
 
-		virtual Integer stable_select(
-			const Mesh &mesh,
-			const Integer element_id) const
-		{
-			//first edge selected
-			return 0;
-		}
+        virtual Integer stable_select(const Mesh &mesh, const Integer element_id) const {
+            // first edge selected
+            return 0;
+        }
 
-		virtual bool repair_element()
-		{
-			return true;
-		}
+        virtual bool repair_element() { return true; }
 
-		virtual bool can_refine(
-			const Mesh &mesh,
-			const Integer element_id) const
-		{
-			return true;
-		}
+        virtual bool can_refine(const Mesh &mesh, const Integer element_id) const { return true; }
 
-		virtual void reorder_edge(
-			const Mesh &mesh,
-			const Integer element_id,
-			Integer &v1,
-			Integer &v2) const
-		{
-		}
+        virtual void reorder_edge(const Mesh &mesh, const Integer element_id, Integer &v1, Integer &v2) const {}
 
-		virtual void edge_refined(
-			const Mesh &mesh,
-			const EdgeElementMap &eem,
-			const Edge &edge)
-		{
-			//
-		}
+        virtual void edge_refined(const Mesh &mesh, const EdgeElementMap &eem, const Edge &edge) {
+            //
+        }
 
-		virtual void update(const Mesh &mesh) {}
+        virtual void update(const Mesh &mesh) {}
 
-		virtual void element_refined(
-			const Mesh &mesh,
-			const Integer element_id,
-			const Edge &edge,
-			const Integer local_midpoint_id)
-		{
-			//do smth
-		}
+        virtual void element_refined(const Mesh &mesh,
+                                     const Integer element_id,
+                                     const Edge &edge,
+                                     const Integer local_midpoint_id) {
+            // do smth
+        }
 
-		virtual bool is_recursive() const
-		{
-			return false;
-		}
+        virtual bool is_recursive() const { return false; }
 
-		virtual void describe(std::ostream &os) const {}
+        virtual void describe(std::ostream &os) const {}
 
-		virtual std::string name() const = 0;
-	};
+        virtual std::string name() const = 0;
+    };
 
-}
+}  // namespace mars
 
-
-#endif //MARS_EDGE_SELECT_HPP
+#endif  // MARS_EDGE_SELECT_HPP
--- kokkos/mars_longest_edge_kokkos.hpp
+++ kokkos/mars_longest_edge_kokkos.hpp
@@ -1,20 +1,23 @@
-#ifndef MARS_LONGEST_EDGE_SELECT_HPP
-#define MARS_LONGEST_EDGE_SELECT_HPP
+#ifndef MARS_LONGEST_EDGE_SELECT_KOKKOS_HPP
+#define MARS_LONGEST_EDGE_SELECT_KOKKOS_HPP
 
 #include "mars_base.hpp"
-#include "mars_edge_select.hpp"
 #include "mars_node_rank.hpp"
 
+#ifdef WITH_KOKKOS
+#include "mars_edge_select_kokkos.hpp"
+#endif
+
 namespace mars {
-    template <class Mesh, class Implementation_>
-    class LongestEdgeSelect final : public EdgeSelect<Mesh, Implementation_> {
+    template <class Mesh>
+    class LongestEdgeSelect<Mesh, KokkosImplementation> final : public EdgeSelect<Mesh, KokkosImplementation> {
     public:
         using Edge = typename Mesh::Edge;
 
-        LongestEdgeSelect(const bool recursive = true, const bool use_tollerance = true)
+        MARS_INLINE_FUNCTION LongestEdgeSelect(const bool recursive = true, const bool use_tollerance = true)
             : recursive_(recursive), use_tollerance_(use_tollerance) {}
 
-        Integer select(const Mesh &mesh, const Integer element_id) const override {
+        MARS_INLINE_FUNCTION Integer select(const Mesh &mesh, const Integer element_id) const override {
             const auto &e = mesh.elem(element_id);
 
             Integer edge_num = 0;
@@ -38,6 +41,7 @@ namespace mars {
             return edge_num;
         }
 
+        MARS_INLINE_FUNCTION
         Integer stable_select(const Mesh &mesh, const Integer element_id) const override {
             const auto &e = mesh.elem(element_id);
 
@@ -74,7 +78,7 @@ namespace mars {
             return edge_num;
         }
 
-        /*
+        /*MARS_INLINE_FUNCTION
         Integer stable_select(const Mesh &mesh, const Integer element_id) const
                         override
         {
@@ -113,6 +117,7 @@ namespace mars {
                 return edge_num;
         }*/
 
+        MARS_INLINE_FUNCTION
         virtual Integer select(const Mesh &mesh, const Edge &neighbor_edge, const Integer element_id) const override {
             if (!use_tollerance_) {
                 return select(mesh, element_id);
@@ -154,7 +159,7 @@ namespace mars {
         // it then selects always the same edge as the stable select with the addition
         // that it does it only for the path with the smallest number of edges which
         // are larger than L.
-        /*
+        /*	MARS_INLINE_FUNCTION
                 Integer select(
                                         const Mesh &mesh,
                                         const Integer element_id,
@@ -241,237 +246,15 @@ namespace mars {
 
         void set_recursive(const bool recursive) { recursive_ = recursive; }
 
-        bool is_recursive() const override { return recursive_; }
+        MARS_INLINE_FUNCTION bool is_recursive() const override { return recursive_; }
 
-        virtual std::string name() const override { return "LongestEdge"; }
-
-    private:
-        bool recursive_;
-        bool use_tollerance_;
-    };
-
-    template <class Mesh>
-    class UniqueLongestEdgeSelect final : public EdgeSelect<Mesh> {
-    public:
-        UniqueLongestEdgeSelect(const bool recursive = true, const bool use_tollerance = true)
-            : recursive_(recursive), use_tollerance_(use_tollerance) {}
-
-        void reorder_edge(const Mesh &mesh, const Integer element_id, Integer &v1, Integer &v2) const override {
-            if (v2 < v1) {
-                std::swap(v1, v2);
-            }
-        }
-
-        Integer select(const Mesh &mesh, const Integer element_id) const override {
-            const auto &e = mesh.elem(element_id);
-            std::vector<std::pair<Edge, Integer> > edge_pairs;
-            edge_pairs.reserve(n_edges(e));
-
-            for (Integer i = 0; i < n_edges(e); ++i) {
-                Integer v1, v2;
-                e.edge(i, v1, v2);
-                edge_pairs.emplace_back(Edge(v1, v2), i);
-            }
-
-            std::sort(edge_pairs.begin(), edge_pairs.end());
-
-            Integer edge_num = 0;
-            Real len = 0;
-
-            for (auto &ep : edge_pairs) {
-                const Integer v1 = ep.first.nodes[0];
-                const Integer v2 = ep.first.nodes[1];
-
-                Real len_i = (mesh.point(v1) - mesh.point(v2)).squared_norm();
-
-                if (len_i > len) {
-                    len = len_i;
-                    edge_num = ep.second;
-                }
-            }
-
-            return edge_num;
-        }
-
-        virtual Integer select(const Mesh &mesh, const Edge &neighbor_edge, const Integer element_id) const override {
-            return select(mesh, element_id);
-        }
-
-        void set_recursive(const bool recursive) { recursive_ = recursive; }
-
-        bool is_recursive() const override { return recursive_; }
-
-        virtual std::string name() const override { return "UniqueLongestEdgeSelect"; }
+        virtual std::string name() const override { return "LongestEdgeKokkos"; }
 
     private:
         bool recursive_;
         bool use_tollerance_;
     };
 
-    template <class Mesh>
-    class GloballyUniqueLongestEdgeSelect final : public EdgeSelect<Mesh> {
-    public:
-        GloballyUniqueLongestEdgeSelect(const Map &map, const bool recursive = true, const bool use_tollerance = true)
-            : map(map),
-              recursive_(recursive),
-              use_tollerance_(use_tollerance)  //, use_trick_(true)
-        {}
-
-        void reorder_edge(const Mesh &mesh, const Integer element_id, Integer &v1, Integer &v2) const override {
-            if (map.global(v2) < map.global(v1)) {
-                std::swap(v1, v2);
-            }
-        }
-
-        bool can_refine(const Mesh &mesh, const Integer element_id) const override {
-            for (auto n : mesh.elem(element_id).nodes) {
-                assert(n != INVALID_INDEX);
-
-                if (map.global(n) == INVALID_INDEX) return false;
-
-                assert(map.local(map.global(n)) != INVALID_INDEX);
-            }
-
-            return true;
-        }
-
-        class EdgeDesc {
-        public:
-            EdgeDesc(const Edge &edge, const Integer edge_num) : edge(edge), edge_num(edge_num) {
-                assert(edge.is_valid());
-            }
-
-            inline bool operator<(const EdgeDesc &other) const { return edge < other.edge; }
-
-            Edge edge;
-            Integer edge_num;
-        };
-
-        Integer select(const Mesh &mesh, const Integer element_id) const override {
-            assert(can_refine(mesh, element_id));
-
-            const auto &e = mesh.elem(element_id);
-            std::vector<EdgeDesc> edge_pairs;
-            edge_pairs.reserve(n_edges(e));
-
-            for (Integer i = 0; i < n_edges(e); ++i) {
-                Integer v1, v2;
-                e.edge(i, v1, v2);
-                edge_pairs.emplace_back(Edge(map.global(v1), map.global(v2)), i);
-            }
-
-            std::sort(edge_pairs.begin(), edge_pairs.end(), [](const EdgeDesc &e1, const EdgeDesc &e2) -> bool {
-                return e1 < e2;
-            });
-
-            Integer edge_num = 0;
-            Real len = 0;
-
-            for (auto &ep : edge_pairs) {
-                const Integer v1 = map.local(ep.edge[0]);
-                const Integer v2 = map.local(ep.edge[1]);
-
-                Real len_i = (mesh.point(v1) - mesh.point(v2)).squared_norm();
-
-                // if(use_trick_) {
-                // 	len_i += ( len_i / ( ep.first[0] + ep.first[1] ) );
-                // }
-
-                if (len_i > len) {
-                    len = len_i;
-                    edge_num = ep.edge_num;
-                }
-            }
-
-            return edge_num;
-        }
-
-        // class EdgeDesc {
-        // public:
-        // 	EdgeDesc(
-        // 		const Edge &edge,
-        // 		const Real len,
-        // 		const Integer edge_num)
-        // 	: edge(edge), len(len), edge_num(edge_num)
-        // 	{
-        // 		assert(edge.is_valid());
-        // 	}
-
-        // 	inline bool operator<(const EdgeDesc &other) const
-        // 	{
-        // 		if(len < other.len) {
-        // 			return true;
-        // 		}
-
-        // 		if(other.len < len) {
-        // 			return false;
-        // 		}
-
-        // 		return edge < other.edge;
-        // 	}
-
-        // 	Edge edge;
-        // 	Real len;
-        // 	Integer edge_num;
-        // };
-
-        // Integer select(
-        // 	const Mesh &mesh,
-        // 	const Integer element_id) const override
-        // {
-        // 	assert(can_refine(mesh, element_id));
-
-        // 	const auto &e = mesh.elem(element_id);
-        // 	std::vector<EdgeDesc> edge_pairs;
-        // 	edge_pairs.reserve(n_edges(e));
-
-        // 	for(Integer i = 0; i < n_edges(e); ++i) {
-        // 		Integer v1, v2;
-        // 		e.edge(i, v1, v2);
-        // 		edge_pairs.emplace_back(
-        // 			Edge(map.global(v1), map.global(v2)),
-        // 			(mesh.point(v1) - mesh.point(v2)).squared_norm(),
-        // 		    i
-        // 		);
-        // 	}
-
-        // 	std::sort(edge_pairs.begin(), edge_pairs.end());
-        // 	return edge_pairs.back().edge_num;
-        // }
-
-        virtual Integer select(const Mesh &mesh, const Edge &neighbor_edge, const Integer element_id) const override {
-            return select(mesh, element_id);
-        }
-
-        void set_recursive(const bool recursive) { recursive_ = recursive; }
-
-        bool is_recursive() const override { return recursive_; }
-
-        virtual std::string name() const override { return "GloballyUniqueLongestEdgeSelect"; }
-
-        void element_refined(const Mesh &mesh,
-                             const Integer element_id,
-                             const Edge &edge,
-                             const Integer local_midpoint_id) override {
-            if (node_rank_) {
-                node_rank_->set_rank_to_midpoint(edge, local_midpoint_id);
-            }
-        }
-
-        void set_node_rank(const std::shared_ptr<NodeRank> &node_rank) { node_rank_ = node_rank; }
-
-        void update(const Mesh &mesh) override {
-            if (node_rank_) {
-                node_rank_->init(mesh);
-            }
-        }
-
-    private:
-        const Map &map;
-        bool recursive_;
-        bool use_tollerance_;
-        std::shared_ptr<NodeRank> node_rank_;
-    };
 }  // namespace mars
 
 #endif  // MARS_LONGEST_EDGE_SELECT_HPP
