5eca50a5b8b166851716758ea0bd838ba7942b0c
Daniel Ganellari <ganellari.d@gmail.com>
2021-01-25 18:13:50 +0100

    First version of the constant viscosity stokes FD with Mars.

--- base/mars_base.hpp
+++ base/mars_base.hpp
@@ -16,6 +16,19 @@ namespace mars {
 
     enum DofLabel : int { lNone = -1, lAll = 0, lVolume = 1, lCorner = 2, lFace = 3 };
 
+    enum VSLabel : int { Diagonal = 0, Right = 1, Left = 2, Up = 3, Down = 4 };
+
+    enum SSXLabel : int {
+        Diagonal = 0, PRight = 1, PLeft = 2, VXRight = 5, VXLeft = 6, VXUp = 7, VXDown = 8;
+        VYUpRight = 9, VYUpLeft = 10, VYDownRight = 11, VYDownLeft = 12
+    };
+
+    enum SSYLabel : int {
+        Diagonal = 0, PUp= 1, PDown= 2, VYRight = 5, VYLeft = 6, VYUp = 7, VYDown = 8;
+        VXUpRight = 9, VXUpLeft = 10, VXDownRight = 11, VXDownLeft = 12
+    };
+
+
     enum ElementType : int {
         // 1D
         Edge2 = 2,
