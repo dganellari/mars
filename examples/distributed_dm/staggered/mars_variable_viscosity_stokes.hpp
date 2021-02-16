cf7fa0121624fbc349481afdfc427b980fb2c259
Daniel Ganellari <ganellari.d@gmail.com>
2021-02-16 13:02:13 +0100

    Assembed an oriented stokes stencil for a variable viscosity problem.

--- examples/distributed_dm/staggered/mars_variable_viscosity_stokes.hpp
+++ examples/distributed_dm/staggered/mars_variable_viscosity_stokes.hpp
@@ -9,6 +9,71 @@ namespace mars {
 
     using namespace stag;
 
+    //Using the orientation feature of the build stencil. In this way there is no need to distinguish between
+    //x and y directions because the stencil is oriented in that way that you need to write the code the same for both x and y dir.
+    template <typename S, typename SP, typename DM>
+    void assemble_oriented_face(S face_stencil, SP sp, const DM &dm) {
+        auto fv_dof_handler = sp.get_dof_handler();
+
+        face_stencil.iterate(MARS_LAMBDA(const Integer stencil_index) {
+            const Integer diag_dof = face_stencil.get_value(stencil_index, SLabel::Diagonal);
+
+            if (!fv_dof_handler.is_boundary_dof(diag_dof)) {
+                auto eta_up_dof = face_stencil.get_value(stencil_index, SSOLabel::VolumeUp);
+                auto eta_up_data = dm.template get_dof_data<IN>(eta_up_dof);
+                auto eta_down_dof = face_stencil.get_value(stencil_index, SSOLabel::VolumeDown);
+                auto eta_down_data = dm.template get_dof_data<IN>(eta_down_dof);
+
+                auto eta_right_dof = face_stencil.get_value(stencil_index, SSOLabel::CornerRight);
+                auto eta_right_data = dm.template get_dof_data<IN>(eta_right_dof);
+                auto eta_left_dof = face_stencil.get_value(stencil_index, SSOLabel::CornerLeft);
+                auto eta_left_data = dm.template get_dof_data<IN>(eta_left_dof);
+
+                sp.set_value(diag_dof, diag_dof, -2 * (eta_up_data + eta_down_data) - eta_right_data - eta_left_data);
+
+                const Integer pu = face_stencil.get_value(stencil_index, SSOLabel::VolumeUp);
+                sp.set_value(diag_dof, pu, -1);
+
+                const Integer pd = face_stencil.get_value(stencil_index, SSOLabel::VolumeDown);
+                sp.set_value(diag_dof, pd, 1);
+
+                const Integer vyr = face_stencil.get_value(stencil_index, SSOLabel::FaceRight);
+                if (vyr == -1) {
+                    sp.set_value(diag_dof, diag_dof, -2 * (eta_up_data + eta_down_data) - eta_left_data);
+                } else {
+                    sp.set_value(diag_dof, vyr, eta_right_data);
+                }
+
+                const Integer vyl = face_stencil.get_value(stencil_index, SSOLabel::FaceLeft);
+                if (vyl == -1) {
+                    sp.set_value(diag_dof, diag_dof, -2 * (eta_up_data + eta_down_data) - eta_right_data);
+                } else {
+                    sp.set_value(diag_dof, vyl, eta_left_data);
+                }
+
+                const Integer vyu = face_stencil.get_value(stencil_index, SSOLabel::FaceUp);
+                sp.set_value(diag_dof, vyu, 2 * eta_up_data);
+
+                const Integer vyd = face_stencil.get_value(stencil_index, SSOLabel::FaceDown);
+                sp.set_value(diag_dof, vyd, 2 * eta_down_data);
+
+                const Integer vxur = face_stencil.get_value(stencil_index, SSOLabel::FaceUpRight);
+                sp.set_value(diag_dof, vxur, eta_right_data);
+
+                const Integer vxul = face_stencil.get_value(stencil_index, SSOLabel::FaceUpLeft);
+                sp.set_value(diag_dof, vxul, -eta_left_data);
+
+                const Integer vxdr = face_stencil.get_value(stencil_index, SSOLabel::FaceDownRight);
+                sp.set_value(diag_dof, vxdr, -eta_right_data);
+
+                const Integer vxdl = face_stencil.get_value(stencil_index, SSOLabel::FaceDownLeft);
+                sp.set_value(diag_dof, vxdl, eta_left_data);
+            }
+        });
+    }
+
+    //same results as in the oriented case just that here we use a not oriented stencil.
+    //and take care of the directions manually.
     template <typename S, typename SP, typename DM>
     void assemble_face(S face_stencil, SP sp, const DM &dm) {
         auto fv_dof_handler = sp.get_dof_handler();
@@ -18,23 +83,23 @@ namespace mars {
 
             if (!fv_dof_handler.is_boundary_dof(diag_dof)) {
                 if (fv_dof_handler.get_orientation(diag_dof) == DofOrient::xDir) {
-                    auto eta_up_dof = face_stencil.get_value(stencil_index, SSXLabel::CornerUp);
+                    auto eta_up_dof = face_stencil.get_value(stencil_index, SSXLabel::CornerXUp);
                     auto eta_up_data = dm.template get_dof_data<IN>(eta_up_dof);
-                    auto eta_down_dof = face_stencil.get_value(stencil_index, SSXLabel::CornerDown);
+                    auto eta_down_dof = face_stencil.get_value(stencil_index, SSXLabel::CornerXDown);
                     auto eta_down_data = dm.template get_dof_data<IN>(eta_down_dof);
 
-                    auto eta_right_dof = face_stencil.get_value(stencil_index, SSXLabel::VolumeRight);
+                    auto eta_right_dof = face_stencil.get_value(stencil_index, SSXLabel::VolumeXRight);
                     auto eta_right_data = dm.template get_dof_data<IN>(eta_right_dof);
-                    auto eta_left_dof = face_stencil.get_value(stencil_index, SSXLabel::VolumeLeft);
+                    auto eta_left_dof = face_stencil.get_value(stencil_index, SSXLabel::VolumeXLeft);
                     auto eta_left_data = dm.template get_dof_data<IN>(eta_left_dof);
 
                     sp.set_value(
                         diag_dof, diag_dof, -2 * (eta_left_data + eta_right_data) - eta_down_data - eta_up_data);
 
-                    const Integer pr = face_stencil.get_value(stencil_index, SSXLabel::VolumeRight);
+                    const Integer pr = face_stencil.get_value(stencil_index, SSXLabel::VolumeXRight);
                     sp.set_value(diag_dof, pr, -1);
 
-                    const Integer pl = face_stencil.get_value(stencil_index, SSXLabel::VolumeLeft);
+                    const Integer pl = face_stencil.get_value(stencil_index, SSXLabel::VolumeXLeft);
                     sp.set_value(diag_dof, pl, 1);
 
                     const Integer vxr = face_stencil.get_value(stencil_index, SSXLabel::FaceXRight);
@@ -70,23 +135,23 @@ namespace mars {
                     sp.set_value(diag_dof, vydl, eta_down_data);
 
                 } else if (fv_dof_handler.get_orientation(diag_dof) == DofOrient::yDir) {
-                    auto eta_up_dof = face_stencil.get_value(stencil_index, SSYLabel::VolumeUp);
+                    auto eta_up_dof = face_stencil.get_value(stencil_index, SSYLabel::VolumeYUp);
                     auto eta_up_data = dm.template get_dof_data<IN>(eta_up_dof);
-                    auto eta_down_dof = face_stencil.get_value(stencil_index, SSYLabel::VolumeDown);
+                    auto eta_down_dof = face_stencil.get_value(stencil_index, SSYLabel::VolumeYDown);
                     auto eta_down_data = dm.template get_dof_data<IN>(eta_down_dof);
 
-                    auto eta_right_dof = face_stencil.get_value(stencil_index, SSYLabel::CornerRight);
+                    auto eta_right_dof = face_stencil.get_value(stencil_index, SSYLabel::CornerYRight);
                     auto eta_right_data = dm.template get_dof_data<IN>(eta_right_dof);
-                    auto eta_left_dof = face_stencil.get_value(stencil_index, SSYLabel::CornerLeft);
+                    auto eta_left_dof = face_stencil.get_value(stencil_index, SSYLabel::CornerYLeft);
                     auto eta_left_data = dm.template get_dof_data<IN>(eta_left_dof);
 
                     sp.set_value(
                         diag_dof, diag_dof, -2 * (eta_up_data + eta_down_data) - eta_left_data - eta_right_data);
 
-                    const Integer pu = face_stencil.get_value(stencil_index, SSYLabel::VolumeUp);
+                    const Integer pu = face_stencil.get_value(stencil_index, SSYLabel::VolumeYUp);
                     sp.set_value(diag_dof, pu, -1);
 
-                    const Integer pd = face_stencil.get_value(stencil_index, SSYLabel::VolumeDown);
+                    const Integer pd = face_stencil.get_value(stencil_index, SSYLabel::VolumeYDown);
                     sp.set_value(diag_dof, pd, 1);
 
                     const Integer vyr = face_stencil.get_value(stencil_index, SSYLabel::FaceYRight);
@@ -156,6 +221,7 @@ namespace mars {
         auto volume_stencil = build_stencil<VStencil>(fv_dof_handler);
         /* print_stencil(fv_dof_handler, volume_stencil); */
 
+        /* auto face_stencil = build_stencil<SStencil, Orient>(fv_dof_handler); */
         auto face_stencil = build_stencil<SStencil>(fv_dof_handler);
         /* print_stencil(fv_dof_handler, face_stencil); */
 
@@ -178,7 +244,7 @@ namespace mars {
 
             printf("lid: %li, u: %lf, global: %li, rank: %i\n", i, idata, d.get_gid(), d.get_proc());
         });
- */
+
 
         cvdm.get_dof_handler().iterate(MARS_LAMBDA(const Integer i) {
             const Integer local_dof = cvdm.get_dof_handler().get_local_dof(i);
@@ -187,10 +253,11 @@ namespace mars {
             Dof d = cvdm.get_dof_handler().local_to_global_dof(local_dof);
 
             printf("lid: %li, u: %lf, global: %li, rank: %i\n", i, idata, d.get_gid(), d.get_proc());
-        });
+        }); */
 
         assemble_volume(volume_stencil, sp, proc_num);
         assemble_face(face_stencil, sp, cvdm);
+        /* assemble_oriented_face(face_stencil, sp, cvdm); */
 
         fv_dof_handler.boundary_dof_iterate(
             MARS_LAMBDA(const Integer local_dof) { sp.set_value(local_dof, local_dof, 1); });
