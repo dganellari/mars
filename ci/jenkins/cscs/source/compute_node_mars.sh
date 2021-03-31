893ec5bb5914a23512c26bf2adad9839ce36625e
Daniel Ganellari <ganellari.d@gmail.com>
2021-03-31 16:20:16 +0200

    Added jenkins pipeline for daint.

---
+++ ci/jenkins/cscs/source/compute_node_mars.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+#SBATCH --job-name=mars_build_jenkins
+#SBATCH --time=01:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=36
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=normal
+#SBATCH --constraint=mc
+#SBATCH --error=/scratch/snx3000/gandanie/build/jenkins-faster-%j.err
+#SBATCH --output=/scratch/snx3000/gandanie/build/jenkins-faster-%j.out
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+set -e
+set -x
+
+id
+ls -lh
+pwd -P
+env
+hostname
+
+DATE=$(date +"%Y%m%d%H%M")
+SCRATCH=/scratch/snx3000/gandanie
+
+for cpu_gpu in cpu ; do
+  mkdir build_${cpu_gpu}
+  pushd build_${cpu_gpu}
+  source $SCRATCH/build/${cpu_gpu}/${cpu_gpu}_jenkins_Release/environment
+  cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_VERBOSE_MAKEFILE=true ..
+  make -j16
+  popd
+done
