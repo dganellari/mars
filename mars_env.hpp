823e107c83b7c89570fb4de7a8921826ee93cba3
zulianp <patrick.zulian@gmail.com>
2021-05-26 15:44:51 +0200

    Added few changes

--- mars_env.hpp
+++ mars_env.hpp
@@ -4,12 +4,19 @@
 #include <memory>
 #include "mars_base.hpp"
 
+#ifdef WITH_MPI
+#include <mpi.h>
+#endif  // WITH_MPI
+
 namespace mars {
     class Env {
     public:
         class Impl;
 
         Env(int argc, char *argv[]);
+#ifdef WITH_MPI
+        Env(int argc, char *argv[], MPI_Comm comm);
+#endif
         ~Env();
         Integer exit_code();
 
