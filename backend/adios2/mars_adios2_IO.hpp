9f638b260d4351caac86710204eec37dad8b27be
zulianp <patrick.zulian@gmail.com>
2021-11-22 09:57:00 +0100

    Added some skeleton example

--- backend/adios2/mars_adios2_IO.hpp
+++ backend/adios2/mars_adios2_IO.hpp
@@ -1,12 +1,20 @@
 #ifndef MARS_ADIOS2_IO_HPP
 #define MARS_ADIOS2_IO_HPP
 
+#include <memory>
+
 namespace mars {
     namespace adios2 {
 
         template <class Mesh>
         class IO {
         public:
+            IO(Mesh &mesh);
+            ~IO();
+
+        private:
+            class Impl;
+            std::unique_ptr<Impl> impl_;
         };
     }  // namespace adios2
 }  // namespace mars
