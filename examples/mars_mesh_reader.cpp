49ad28706de66950ea8f75bbf63ba6d0edb37eca
Dylan Ramelli <rameld@usi.ch>
2022-01-21 17:28:01 +0100

    Added reader files

---
+++ examples/mars_mesh_reader.cpp
@@ -0,0 +1,28 @@
+// #include <cmath>
+// #include <complex>
+// #include <iostream>
+// // #include "adios2.h"
+// #include "cxxopts.hpp"
+// #include "mars_globals.hpp"
+// #include "mars_mandelbrot.hpp"
+// #include "mars_mesh.hpp"
+// #include "mars_mesh_generation.hpp"
+// #include "mars_test_mesh.hpp"
+
+// #ifdef WITH_KOKKOS
+// #include "mars_lepp_benchmark_kokkos.hpp"
+// #include "mars_mesh_kokkos.hpp"
+// #endif
+
+// #if ADIOS2_USE_MPI
+// #include <mpi.h>
+// #endif
+
+// void read(std::string filename) {}
+
+// int main(int argc, char const *argv[]) {
+//     std::string inputFileName;
+//     if (argc == 2) {
+//         inputFileName = argv[1];
+//     }
+// }
