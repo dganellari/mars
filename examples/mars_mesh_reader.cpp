5cec554928e2aa41b8ebfa9a277c897a43c619e5
Dylan Ramelli <rameld@usi.ch>
2022-01-28 21:59:41 +0100

    Printing infor of mesh

--- examples/mars_mesh_reader.cpp
+++ examples/mars_mesh_reader.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <complex>
 #include <iostream>
+#include <map>
 #include "adios2.h"
 #include "cxxopts.hpp"
 #include "mars_globals.hpp"
@@ -25,12 +26,23 @@ void read(std::string filename) {
     reader = io_main.Open(filename, adios2::Mode::Read);
     reader.BeginStep();
 
+    const std::map<std::string, adios2::Params> variables = io_main.AvailableVariables();
+
+    for (const auto variablePair : variables) {
+        std::cout << "Name: " << variablePair.first;
+
+        for (const auto &parameter : variablePair.second) {
+            std::cout << "\t" << parameter.first << ": " << parameter.second << "\n";
+        }
+    }
+
     adios2::Variable<double> uVar = io_main.InquireVariable<double>("U");
     adios2::Attribute<std::string> uAttr = io_main.InquireAttribute<std::string>("format/mars_mesh");
     adios2::Variable<uint32_t> numOfElements = io_main.InquireVariable<uint32_t>("NumOfElements");
     if (uVar) {
         std::vector<double> example;
         std::cout << "Got it\n";
+
         reader.Get(uVar, example, adios2::Mode::Deferred);
         std::cout << example.size() << std::endl;
     }
@@ -42,4 +54,13 @@ int main(int argc, char const *argv[]) {
     if (argc == 2) {
         inputFileName = argv[1];
     }
+
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    read(inputFileName);
+
+    MPI_Finalize();
 }
