4b5e13ae19751f7a3772b728d2e7079a0ac22caa
Dylan Ramelli <rameld@usi.ch>
2022-02-07 11:12:12 +0100

    Getting works but returns 0 value

--- examples/mars_mesh_reader.cpp
+++ examples/mars_mesh_reader.cpp
@@ -1,8 +1,8 @@
+#include <adios2.h>
 #include <cmath>
 #include <complex>
 #include <iostream>
 #include <map>
-#include "adios2.h"
 #include "cxxopts.hpp"
 #include "mars_globals.hpp"
 #include "mars_mandelbrot.hpp"
@@ -14,15 +14,13 @@
 #include "mars_lepp_benchmark_kokkos.hpp"
 #include "mars_mesh_kokkos.hpp"
 #endif
-
-#if ADIOS2_USE_MPI
 #include <mpi.h>
-#endif
 
-void read(std::string filename) {
+void read(std::string filename, int rank) {
     adios2::ADIOS adios(adios2::DebugON);
     adios2::IO io_main = adios.DeclareIO("SimulationInput");
     adios2::Engine reader;
+
     reader = io_main.Open(filename, adios2::Mode::Read);
     reader.BeginStep();
 
@@ -36,20 +34,28 @@ void read(std::string filename) {
         }
     }
 
+    const size_t Nx = 10;
     adios2::Variable<double> uVar = io_main.InquireVariable<double>("U");
     adios2::Attribute<std::string> uAttr = io_main.InquireAttribute<std::string>("format/mars_mesh");
     adios2::Variable<uint32_t> numOfElements = io_main.InquireVariable<uint32_t>("NumOfElements");
     if (uVar) {
         std::vector<double> myU;
-        std::cout << "Got it\n";
+        std::cout << "Got variable U\n";
 
-        reader.Get(uVar, myU, adios2::Mode::Deferred);
-        std::cout << myU.size() << std::endl;
+        uVar.SetSelection({{Nx * rank}, {Nx}});
+
+        reader.Get(uVar, myU, adios2::Mode::Sync);
+        for (const auto number : myU) {
+            std::cout << number << " ";
+        }
+        std::cout << "\n";
     }
     reader.EndStep();
+
+    reader.Close();
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
     std::string inputFileName;
     if (argc == 2) {
         inputFileName = argv[1];
@@ -60,7 +66,7 @@ int main(int argc, char const *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    read(inputFileName);
+    read(inputFileName, rank);
 
     MPI_Finalize();
 }
