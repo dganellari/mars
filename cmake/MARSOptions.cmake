5a3d58dbbf2a9184490b0cc801be1e8bc3dbbf3d
Daniel Ganellari <ganellari.d@gmail.com>
2022-05-18 19:45:48 +0300

    First draft of porting mars to HIP AMD GPUs.

--- cmake/MARSOptions.cmake
+++ cmake/MARSOptions.cmake
@@ -1,7 +1,6 @@
 option(MARS_ENABLE_VTK "Uses VTK IO" OFF)
 option(MARS_ENABLE_ADIOS2 "Uses ADIOS2 IO" OFF)
-option(MARS_USE_CUDA "Build mars with cuda support" OFF)
+option(MARS_USE_CUDA "Build mars with CUDA support" OFF)
 option(MARS_NO_RDMA "Build mars with NO RDMA support" OFF)
-option(MARS_USE_CUDA "Build mars with cuda support" OFF)
 option(MARS_USE_CUDAUVM "Use as default memory space CUDAUVM. This has only an effect if MARS_USE_CUDA is used too." OFF)
-
+option(MARS_USE_HIP "Build mars with HIP support" OFF)
