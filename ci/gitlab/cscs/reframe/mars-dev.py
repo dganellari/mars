0dec1573f7be1648a8682f0bd3d6b82c8c059e0f
Daniel Ganellari <ganellari.d@gmail.com>
2024-12-06 15:06:32 +0100

    remove space

--- ci/gitlab/cscs/reframe/mars-dev.py
+++ ci/gitlab/cscs/reframe/mars-dev.py
@@ -96,7 +96,7 @@ class mars_build(rfm.CompileOnlyRegressionTest):
 @rfm.simple_test
 class mars_unit(rfm.RunOnlyRegressionTest):
     descr = 'Run the mars unit tests'
-    valid_systems = ['+uenv ']
+    valid_systems = ['uenv']
     valid_prog_environs = ['+mpi']
     target_executable = variable(str, value='ctest')
     time_limit = '5m'
