086675268b683a04fd8b1d02d13ca8f90deb7509
Daniel Ganellari <ganellari.d@gmail.com>
2024-12-06 15:30:27 +0100

    remove kokkos

--- ci/gitlab/cscs/uenv/reframe/mars-dev.py
+++ ci/gitlab/cscs/uenv/reframe/mars-dev.py
@@ -62,7 +62,7 @@ class mars_download(rfm.RunOnlyRegressionTest):
 class mars_build(rfm.CompileOnlyRegressionTest):
     descr = 'Build MARS'
     valid_systems = ['*']
-    valid_prog_environs = ['+mpi +kokkos']
+    valid_prog_environs = ['+mpi']
     build_system = 'CMake'
     sourcedir = None
     maintainers = ['dganellari']
@@ -112,7 +112,7 @@ class mars_build(rfm.CompileOnlyRegressionTest):
 @rfm.simple_test
 class mars_unit(rfm.RunOnlyRegressionTest):
     descr = 'Run the mars unit tests'
-    valid_systems = ['+uenv']
+    valid_systems = ['*']
     valid_prog_environs = ['+mpi']
     target_executable = variable(str, value='ctest')
     time_limit = '5m'
@@ -134,7 +134,7 @@ class mars_unit(rfm.RunOnlyRegressionTest):
 @rfm.simple_test
 class mars_discretization(rfm.RunOnlyRegressionTest):
     descr = 'Run the mars FEM discretization tests small and medium mesh'
-    valid_systems = ['+uenv']
+    valid_systems = ['*']
     valid_prog_environs = ['+mpi']
     target_executable = variable(str, value='ctest')
     maintainers = ['dganellari']
